840d6b77a8c105cf9b57ce9da4dc7bd9e0c0cf9e
cafesun <cafesun_global@163.com>
2018-04-16 23:46:38 +0800

    init snmp example

---
+++ ExSNMP/demo_agentx/src/main/java/com/solartech/ex/snmp/ExAgentXDemo.java
@@ -0,0 +1,8 @@
+package com.solartech.ex.snmp;
+
+public class ExAgentXDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ ExSNMP/demo_snmp4j/src/main/java/com/solartech/ex/snmp/ExSnmp4JDemo.java
@@ -0,0 +1,8 @@
+package com.solartech.ex.snmp;
+
+public class ExSnmp4JDemo {
+
+    public static void main(String[] args) {
+
+    }
+}

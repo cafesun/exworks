cf561981a30bb175a1da84729cdf2968890c7b74
cafesun <cafesun_global@163.com>
2018-05-02 23:09:40 +0800

    增加采集CPU利用率的方法

---
+++ ExEffectiveJava/chapter02/src/main/java/com/solartech/ex/ExItem6_WeakHashMap.java
@@ -0,0 +1,11 @@
+package com.solartech.ex;
+
+import java.util.WeakHashMap;
+import java.util.Map;
+
+public class ExItem6_WeakHashMap {
+
+    public static void main(String[] args) {
+        Map<Integer, String> oHashMap = new WeakHashMap();
+    }
+}

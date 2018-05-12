e5e53f5d91cb76084d5ff5857d67e778dd3d1acc
cafesun <cafesun_global@163.com>
2018-05-12 23:06:43 +0800

    增加Chapter6的POM

--- ExEffectiveJava/chapter02/src/main/java/com/solartech/ex/ExItem6_WeakHashMap.java
+++ ExEffectiveJava/chapter02/src/main/java/com/solartech/ex/ExItem6_WeakHashMap.java
@@ -6,6 +6,7 @@ import java.util.Map;
 public class ExItem6_WeakHashMap {
 
     public static void main(String[] args) {
-        Map<Integer, String> oHashMap = new WeakHashMap();
+        Map<Integer, String> oHashMap = new WeakHashMap<>();
+
     }
 }
